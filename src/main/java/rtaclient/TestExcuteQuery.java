8ca60778b16b4c0540c1fb5802168b71583188b2
mickeyjoe666 <mickeyjoe666@gmail.com>
2018-07-06 17:19:04 +0900

    test for slack log

--- src/main/java/rtaclient/TestExcuteQuery.java
+++ src/main/java/rtaclient/TestExcuteQuery.java
@@ -8,7 +8,7 @@ public class TestExcuteQuery {
 	public static void main(String[] args) {
 		GlobalEnv.setGlobalEnv(args);
 		RTAClient newuser = new RTAClient();
-		String query =  "SELECT p.prefecture_name, p.city_name, p.address FROM #postal_code p;";
+		String query =  "SELECT p.prefecture_name, p.city_name, p.address FROM #postal_code p;";//sample query
 
 		newuser.excuteQuery(query);// TODO Auto-generated method stub
 //		DBTablePrinter.printResultSet(newuser.rs_tmp);
